0ef61ac7fad9887467382c3112a3663242d4b624
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2023-09-19 22:14:41 -0400

    Created function for output dir.

--- src/services/file_service.py
+++ src/services/file_service.py
@@ -1,4 +1,21 @@
+import os
 from pathlib import Path
+
+
+def get_output_dir(**kwargs):
+    path = get_path("output")
+
+    for key, value in kwargs.items():
+        subdir_name = f"{key}_{value}"
+
+        # Construct the full path of the subdirectory
+        path = path.joinpath(path, subdir_name)
+
+        # Create the subdirectory
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    return path
 
 
 def get_path(*args: str) -> Path:
