7ef24eaab16dceafdfae8cd863402a68895646c1
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2023-10-23 14:23:48 -0400

    Cleaned up gamma_data_prep script.

--- rsrc/hmc_calibration/gamma_data_prep.R
+++ rsrc/hmc_calibration/gamma_data_prep.R
@@ -2,14 +2,13 @@ library(tidyverse)
 library(sf)
 
 # DATA INPUT
-# load variables at the muni level to calibrate theta
 load("data/calibration/prepData/muniTheta_prepData_gamma.Rdata")
 
+# Convert biomass into CO2e, add column of ones, take logs, and scale
 df <- muniTheta.prepData %>%
   mutate(co2e_ha_2017 = (agb_2017 / 2) * (44 / 12)) %>%
-  select(historical_precip, historical_temp, lat, lon, co2e_ha_2017) %>%
   mutate(
-
+    X1 = 1,
     log_historical_precip = log(historical_precip),
     log_historical_temp = log(historical_temp),
     log_lat = log(lat),
@@ -21,23 +20,15 @@ df <- muniTheta.prepData %>%
     log_historical_temp = scale(log_historical_temp),
     log_lat = scale(log_lat),
     log_lon = scale(log_lon)
-  )
+  ) %>%
+  select(X1,
+         log_historical_precip,
+         log_historical_temp,
+         log_lat,
+         log_lon,
+         log_co2e_ha_2017)
 
-
-
-reg_gamma_2017 <-
-  df  %>%
-  lm(
-    formula = log_co2e_ha_2017  ~ log_historical_precip + log_historical_temp + log_lat + log_lon,
-    na.action = na.exclude
-  )
-
-reg_summary <- summary(reg_gamma_2017)
-regressor_df <- as.data.frame(reg_gamma_2017$model[-1])
-
-df <- cbind(1, df)
-df <- df %>%
-  select(1, log_historical_precip,log_historical_temp,  log_lat, log_lon, log_co2e_ha_2017)
-
-
-st_write(df, "data/hmc/data_gamma.geojson", driver = "GeoJSON", delete_dsn = TRUE)
+st_write(df,
+         "data/hmc/data_gamma.geojson",
+         driver = "GeoJSON",
+         delete_dsn = TRUE)
