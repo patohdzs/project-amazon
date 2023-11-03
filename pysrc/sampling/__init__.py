34be27924c2036ba440829438159b5ba15d35ad8
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2023-11-03 01:29:21 -0400

    Added area-share weighting to G matrices.

--- pysrc/sampling/__init__.py
+++ pysrc/sampling/__init__.py
@@ -2,18 +2,17 @@ import numpy as np
 
 
 def theta_adj_reg_data(num_sites, theta_df):
-    # Filter out null values
-    theta_df = theta_df[theta_df["zbar_2017_muni"].notna()]
-
     # Get design matrix and its dimensions
     X = theta_df.iloc[:, :8].to_numpy()
     N, K = X.shape
 
-    # Get weighted grouped average matrix
+    # Get site indicator matrix
     G = np.array(
         [(theta_df["id"].to_numpy() == i).astype(int) for i in range(1, num_sites + 1)]
     )
-    G = theta_df["zbar_2017_muni"].to_numpy() * G
+
+    # Multiply by area overalp weights
+    G = theta_df["muni_site_area"].to_numpy() * G
     G = G / G.sum(axis=1, keepdims=True)
 
     return {
@@ -33,6 +32,9 @@ def gamma_adj_reg_data(num_sites, gamma_df):
     G = np.array(
         [(gamma_df["id"].to_numpy() == i).astype(int) for i in range(1, num_sites + 1)]
     )
+
+    # Multiply by area overalp weights
+    G = gamma_df["muni_site_area"].to_numpy() * G
     G = G / G.sum(axis=1, keepdims=True)
 
     return {
