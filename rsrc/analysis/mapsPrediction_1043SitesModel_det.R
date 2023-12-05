c67da3cae1fe976a1d2b981c8fb470264fef3ce6
Pengyu <pengyu@uchicago.edu>
2023-12-05 14:32:18 -0600

    adjust the shadow price

--- rsrc/analysis/mapsPrediction_1043SitesModel_det.R
+++ rsrc/analysis/mapsPrediction_1043SitesModel_det.R
@@ -51,7 +51,7 @@ load(here::here("data/hmc", "hmc_1043SitesModel.Rdata"))
 
 
 # 1043 SITES AGGREGATE PREDICTION
-aux.prices <- c(7.9, 17.9, 22.9, 27.9)
+aux.prices <- c(8.0, 18.0, 23.0, 28.0)
 
 
 
@@ -122,7 +122,7 @@ clean.amazonBiome <- sf::st_transform(clean.amazonBiome, sf::st_crs(prediction.1
 # z_2017_1043Sites
 z_2017_1043Sites <-
     ggplot2::ggplot(data = prediction.1043SitesModel %>%
-                      dplyr::filter(time == 0, p_e == 22.9) %>%
+                      dplyr::filter(time == 0, p_e == 23.0) %>%
                       dplyr::mutate(z_t = cut(round(z_t,1),
                                               breaks = c(0,0.5,20,40,60,80,105),
                                               include.lowest = T,
@@ -146,7 +146,7 @@ z_2017_1043Sites <-
 
 # gamma_1043Sites
 gamma_1043Sites <-
-  ggplot2::ggplot(data = prediction.1043SitesModel %>% dplyr::filter(time == 0, p_e == 7.9) %>%
+  ggplot2::ggplot(data = prediction.1043SitesModel %>% dplyr::filter(time == 0, p_e == 8.0) %>%
                     dplyr::mutate(rank_gamma_1043Sites = cut(round(rank_gamma_1043Sites), breaks = c(1, 212, 423, 634, 845, 1043), include.lowest = T, dig.lab = 4))) +
   ggplot2::geom_sf(aes(fill = rank_gamma_1043Sites)) +
   ggplot2::scale_fill_brewer(name = expression(paste(gamma^"i", ~"(rank)")), palette = "YlOrRd", direction = -1) +
@@ -165,7 +165,7 @@ gamma_1043Sites <-
 
 # theta_1043Sites
 theta_1043Sites <-
-  ggplot2::ggplot(data = prediction.1043SitesModel %>% dplyr::filter(time == 0, p_e == 7.9) %>%
+  ggplot2::ggplot(data = prediction.1043SitesModel %>% dplyr::filter(time == 0, p_e == 8.0) %>%
                     dplyr::mutate(rank_theta_1043Sites = cut(round(rank_theta_1043Sites), breaks = c(1, 212, 423, 634, 845, 1043), include.lowest = T, dig.lab = 4))) +
   ggplot2::geom_sf(aes(fill = rank_theta_1043Sites)) +
   ggplot2::scale_fill_brewer(name = expression(paste(theta^"i", ~"(rank)")), palette = "YlOrRd", direction = -1) +
@@ -187,7 +187,7 @@ theta_1043Sites <-
 mapList <- list()
 mapIndex <- 1
 for (price in aux.prices) {
-  transfer <- price-7.9
+  transfer <- price-8.0
   # z50 map (vary by model)
   mapList[[mapIndex]] <-
     ggplot2::ggplot(data = prediction.1043SitesModel %>%
@@ -239,7 +239,7 @@ aux.years <- c(2017, 2022, 2027, 2032, 2037, 2067)
 for (p in seq_along(aux.prices)) {
 
   aux.mapList <- list()
-  transfer <- aux.prices[p]-7.9
+  transfer <- aux.prices[p]-8.0
 
   for (y in seq_along(aux.years)) {
 
@@ -289,14 +289,14 @@ for (y in seq_along(aux.years)) {
 
   # z50 map (vary by model)
     ggplot2::ggplot(data = prediction.1043SitesModel %>%
-                      dplyr::filter(time == aux.years[y]-2017, p_e == 22.9) %>%
+                      dplyr::filter(time == aux.years[y]-2017, p_e == 23.0) %>%
                       dplyr::mutate(z_t = cut(round(z_t,2),
                                               breaks = c(0,0.5,20,40,60,80,105),
                                               include.lowest = T,
                                               dig.lab = 3,
                                               labels = c("[0]", "(0-20]", "(20-40]", "(40-60]", "(60-80]", "(80-100]")))) +
     ggplot2::geom_sf(aes(fill = z_t)) +
-    ggplot2::scale_fill_manual(name = expr(paste("Z"[!!aux.years[y]]^"i", ~"(%), ", "P"^"e", "=", 22.9)), values = c("white", RColorBrewer::brewer.pal(5, "YlOrRd")), drop = FALSE) +
+    ggplot2::scale_fill_manual(name = expr(paste("Z"[!!aux.years[y]]^"i", ~"(%), ", "P"^"e", "=", 23.0)), values = c("white", RColorBrewer::brewer.pal(5, "YlOrRd")), drop = FALSE) +
     ggplot2::geom_sf(data = clean.amazonBiome, fill = NA, color = "darkgreen", size = 1.2) +
     ggplot2::guides(fill = guide_legend(label.position = "bottom", title.position = "top", nrow = 1)) +
     ggplot2::theme(panel.grid.major = element_line(colour = "white"),
