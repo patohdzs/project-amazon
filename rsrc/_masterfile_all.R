bd0aec488c5676b8ff40cf4a69118a2190e2d5a9
Pengyu <pengyu@uchicago.edu>
2024-03-21 11:15:45 -0500

    set a masterfile for all

---
+++ rsrc/_masterfile_all.R
@@ -0,0 +1,34 @@
+
+# > THIS SCRIPT
+# AIM: MASTERFILE SCRIPT TO ALL R data scripts
+
+source("rsrc/setup.R")
+
+# START TIMER
+tictoc::tic(msg = "_masterfile.R script", log = T)
+
+source(here::here("rsrc/raw2clean/_masterfile_raw2clean.R"), encoding = "UTF-8", echo = T)
+
+# clear environment
+rm(list = ls())
+
+
+source(here::here("rsrc/calibration/_masterfile_prep.R"), encoding = "UTF-8", echo = T)
+
+# clear environment
+rm(list = ls())
+
+
+source(here::here("rsrc/calibration/_masterfile_calibration.R"), encoding = "UTF-8", echo = T)
+
+# clear environment
+rm(list = ls())
+
+
+
+# END TIMER
+tictoc::toc(log = T)
+
+
+
+# END OF SCRIPT --------------------------------------------------------------------------------------------------------------------------------------
