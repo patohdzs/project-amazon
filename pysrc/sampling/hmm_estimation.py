067488f76b21007193da5e798361d09a5c63d6f7
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-04-25 13:51:29 -0400

    Refactoring, formatting and dependencies for HMM.

---
+++ pysrc/sampling/hmm_estimation.py
@@ -0,0 +1,99 @@
+import os
+
+import numpy as np
+import pandas as pd
+from hmmlearn import hmm
+from scipy.linalg import expm, logm
+
+
+def estimate_price_model(dist, num_iterations=5, var="uncon"):
+    for i in range(num_iterations):
+        s_low, s_high = dist[0], dist[1]
+
+        # Estimate HMM transitions and states
+        aic, ll, bic, mus, sigmas, P = _estimate(s_low, s_high, var)
+
+        # Compute stationary distribution
+        dist, sta_price = _stationary_distribution(P, mus)
+
+        # Annualize transition matrix
+        annual_P = _annualize_transition_matrix(P)
+
+    return aic, ll, bic, mus, sigmas, P, dist, sta_price, annual_P
+
+
+def _estimate(s_low=0.5, s_high=0.5, var="uncon"):
+    # need to input initial state prob s_low and s_high
+    data_folder = os.getcwd() + "/data/hmc/"
+
+    # read data
+    df = pd.read_csv(data_folder + "seriesPriceCattle_prepared.csv")
+    price = df["price_real_mon_cattle"].values.astype(float)
+    logprice = np.log(price)
+
+    # estimating the model
+
+    Q = logprice.reshape(logprice.shape[0], 1)
+
+    np.random.seed(123)
+
+    best_score = best_model = None
+    n_fits = 500
+
+    for idx in range(n_fits):
+        if var == "uncon":
+            model = hmm.GaussianHMM(
+                n_components=2, random_state=idx, init_params="tmc", params="stmc"
+            )
+        else:
+            model = hmm.GaussianHMM(
+                n_components=2,
+                random_state=idx,
+                init_params="tmc",
+                params="stmc",
+                covariance_type="tied",
+            )
+
+        model.startprob_ = np.array([s_low, s_high])
+        model.fit(Q)
+        score = model.score(Q)
+
+        if best_score is None or score > best_score:
+            best_model = model
+            best_score = score
+
+    aic = best_model.aic(Q)
+    bic = best_model.bic(Q)
+    mus = np.exp(np.ravel(best_model.means_))
+    sigmas = np.ravel(np.sqrt([np.diag(c) for c in best_model.covars_]))
+    P = best_model.transmat_
+
+    sorted_indices = np.argsort(mus)
+    mus_sorted = mus[sorted_indices]
+    sigmas_sorted = sigmas[sorted_indices]
+    P_sorted = P[sorted_indices][:, sorted_indices]
+
+    if var == "uncon":
+        ll = (best_model.aic(Q) - 12) / (-2)
+    else:
+        ll = (best_model.aic(Q) - 10) / (-2)
+
+    return (aic, ll, bic, mus_sorted, sigmas_sorted, P_sorted)
+
+
+def _stationary_distribution(transition_matrix, mus):
+    eigenvals, eigenvects = np.linalg.eig(transition_matrix.T)
+    close_to_1_idx = np.isclose(eigenvals, 1)
+    target_eigenvect = eigenvects[:, close_to_1_idx]
+    target_eigenvect = target_eigenvect[:, 0]
+    stationary_distrib = target_eigenvect / sum(target_eigenvect)
+    stationary_price = mus[0] * stationary_distrib[0] + mus[1] * stationary_distrib[1]
+    return (stationary_distrib, stationary_price)
+
+
+def _annualize_transition_matrix(transition_matrix):
+    dτ = 1 / 12  # time step
+    P = transition_matrix  # probability transition matrix
+    M = logm(P) / dτ  # instantenous generator
+    P = expm(M)  # Updated Probability transition matrix
+    return P
--- pysrc/sampling/mpc.py
+++ pysrc/sampling/mpc.py
@@ -1,80 +1,103 @@
-import numpy as np
-import pandas as pd
 import os
 import shutil
+
+import numpy as np
+import pandas as pd
+
+from pysrc.sampling import baseline, hmm_estimation
+
 from ..services.data_service import load_site_data
-from pysrc.sampling import baseline
-from pysrc.sampling import mpc_estimation
+
 
 def mc_samples_constrained(location):
-    
     np.random.seed(123)
 
-    aic, ll, bic, mus, sigmas, P, sta_dist, sta_price,annual_P = mpc_estimation.iteration_est([0.5, 0.5], num_iterations=5, var='con')
-    
+    (
+        aic,
+        ll,
+        bic,
+        mus,
+        sigmas,
+        P,
+        sta_dist,
+        sta_price,
+        annual_P,
+    ) = hmm_estimation.estimate_price_model([0.5, 0.5], num_iterations=5, var="con")
+
     num_simulations = 200
 
-    states = {'low': mus[0], 'high': mus[1]}
-    initial_state = 'high' 
+    states = {"low": mus[0], "high": mus[1]}
+    initial_state = "high"
 
     probability_matrix = {
-        'low': {'low': annual_P[0,0], 'high': annual_P[0,1]},
-        'high': {'low': annual_P[1,0], 'high': annual_P[1,1]}
+        "low": {"low": annual_P[0, 0], "high": annual_P[0, 1]},
+        "high": {"low": annual_P[1, 0], "high": annual_P[1, 1]},
     }
 
     # Number of observations
     num_observations = 200
 
-
     for i in range(1, num_simulations + 1):
-        
         # Generating the Markov chain for each simulation
         current_state = initial_state
         markov_chain = [states[current_state]]
 
         for _ in range(num_observations - 1):
-            next_state = np.random.choice(list(probability_matrix[current_state].keys()), 
-                                        p=list(probability_matrix[current_state].values()))
+            next_state = np.random.choice(
+                list(probability_matrix[current_state].keys()),
+                p=list(probability_matrix[current_state].values()),
+            )
             markov_chain.append(states[next_state])
             current_state = next_state
 
-        transformed_markov_chain = [1 if price == mus[0] else 2 for price in markov_chain]
+        transformed_markov_chain = [
+            1 if price == mus[0] else 2 for price in markov_chain
+        ]
 
         # Creating an index from 1 to 200
         index = range(1, num_observations + 1)
 
         # Combine index and transformed Markov chain into a DataFrame
-        markov_chain_df = pd.DataFrame({'Index': index, 'scenario': transformed_markov_chain})
+        markov_chain_df = pd.DataFrame(
+            {"Index": index, "scenario": transformed_markov_chain}
+        )
 
-        # Specify the filename for each CSV file (e.g., mc_1.csv, mc_2.csv, ..., mc_100.csv)
-        csv_filename = f'/mc_{i}.csv'
-        output=location
+        # Specify the filename for each CSV file
+        csv_filename = f"/mc_{i}.csv"
+        output = location
         # Output to CSV file
-        markov_chain_df.to_csv(output+csv_filename, index=False)
-    
-    
-    return("mc sampling is done")
+        markov_chain_df.to_csv(output + csv_filename, index=False)
+
+    return "mc sampling is done"
 
 
 def mc_samples_unconstrained(location):
-    
     np.random.seed(123)
 
-    aic, ll, bic, mus, sigmas, P, sta_dist, sta_price,annual_P = mpc_estimation.iteration_est([0.5, 0.5], num_iterations=5, var='uncon')
-    
+    (
+        aic,
+        ll,
+        bic,
+        mus,
+        sigmas,
+        P,
+        sta_dist,
+        sta_price,
+        annual_P,
+    ) = hmm_estimation.estimate_price_model([0.5, 0.5], num_iterations=5, var="uncon")
+
     num_simulations = 200
 
-    states = {'low': mus[0], 'high': mus[1]}
-    initial_state = 'high' 
+    states = {"low": mus[0], "high": mus[1]}
+    initial_state = "high"
 
     probability_matrix = {
-        'low': {'low': annual_P[0,0], 'high': annual_P[0,1]},
-        'high': {'low': annual_P[1,0], 'high': annual_P[1,1]}
+        "low": {"low": annual_P[0, 0], "high": annual_P[0, 1]},
+        "high": {"low": annual_P[1, 0], "high": annual_P[1, 1]},
     }
 
     # Number of observations
     num_observations = 200
-
 
     for i in range(1, num_simulations + 1):
         # Generating the Markov chain for each simulation
@@ -82,33 +105,37 @@ def mc_samples_unconstrained(location):
         markov_chain = [states[current_state]]
 
         for _ in range(num_observations - 1):
-            next_state = np.random.choice(list(probability_matrix[current_state].keys()), 
-                                        p=list(probability_matrix[current_state].values()))
+            next_state = np.random.choice(
+                list(probability_matrix[current_state].keys()),
+                p=list(probability_matrix[current_state].values()),
+            )
             markov_chain.append(states[next_state])
             current_state = next_state
 
-        transformed_markov_chain = [1 if price == mus[0] else 2 for price in markov_chain]
+        transformed_markov_chain = [
+            1 if price == mus[0] else 2 for price in markov_chain
+        ]
 
         # Creating an index from 1 to 200
         index = range(1, num_observations + 1)
 
         # Combine index and transformed Markov chain into a DataFrame
-        markov_chain_df = pd.DataFrame({'Index': index, 'scenario': transformed_markov_chain})
+        markov_chain_df = pd.DataFrame(
+            {"Index": index, "scenario": transformed_markov_chain}
+        )
 
-        # Specify the filename for each CSV file (e.g., mc_1.csv, mc_2.csv, ..., mc_100.csv)
-        csv_filename = f'/mc_{i}.csv'
-        output=location
+        # Specify the filename for each CSV file
+        csv_filename = f"/mc_{i}.csv"
+        output = location
         # Output to CSV file
-        markov_chain_df.to_csv(output+csv_filename, index=False)
+        markov_chain_df.to_csv(output + csv_filename, index=False)
 
-    return("mc sampling is done")
+    return "mc sampling is done"
 
 
-
-def gdx_files(location,num_sites=78):
+def gdx_files(location, num_sites=78):
     # Load site data
-    
-    
+
     (
         zbar_2017,
         z_2017,
@@ -118,21 +145,19 @@ def gdx_files(location,num_sites=78):
         _,
         _,
     ) = load_site_data(num_sites)
-        
-    
+
     baseline_fit = baseline.sample(
-    model_name="full_model",
-    num_sites=num_sites,
-    iter_sampling=10**4,
-    chains=5,
-    seed=1,
+        model_name="full_model",
+        num_sites=num_sites,
+        iter_sampling=10**4,
+        chains=5,
+        seed=1,
     )
 
     theta = baseline_fit.stan_variable("theta").mean(axis=0)
     gamma = baseline_fit.stan_variable("gamma").mean(axis=0)
-    
-    
-    scale=1e9
+
+    scale = 1e9
     # Computing carbon absorbed in start period
     x0_vals = gamma * forest_area_2017
 
@@ -141,89 +166,84 @@ def gdx_files(location,num_sites=78):
     zbar_2017 = zbar_2017 * scale
 
     x0data = pd.DataFrame(x0_vals)
-    x0data.columns=['x_2017_78Sites']
+    x0data.columns = ["x_2017_78Sites"]
     x0data.index = x0data.index + 1
     saveto = os.path.join(location, "X0Data.csv")
     x0data.to_csv(saveto)
 
     z0data = pd.DataFrame(site_z_vals)
-    z0data.columns=['z_2017_78Sites']
+    z0data.columns = ["z_2017_78Sites"]
     z0data.index = z0data.index + 1
     saveto = os.path.join(location, "Z0Data.csv")
     z0data.to_csv(saveto)
 
     zbardata = pd.DataFrame(zbar_2017)
-    zbardata.columns=['zbar_2017_78Sites']
+    zbardata.columns = ["zbar_2017_78Sites"]
     zbardata.index = zbardata.index + 1
     saveto = os.path.join(location, "ZbarData.csv")
     zbardata.to_csv(saveto)
 
     gammadata = pd.DataFrame(gamma)
-    gammadata.columns = ['gamma_78Sites']
+    gammadata.columns = ["gamma_78Sites"]
     gammadata.index = gammadata.index + 1
     saveto = os.path.join(location, "GammaData.csv")
     gammadata.to_csv(saveto)
 
     thetadata = pd.DataFrame(theta)
-    thetadata.columns=['theta_78Sites']
+    thetadata.columns = ["theta_78Sites"]
     thetadata.index = thetadata.index + 1
     saveto = os.path.join(location, "ThetaData.csv")
     thetadata.to_csv(saveto)
 
-    return("gdx file is done")
+    return "gdx file is done"
 
 
-def paste_file(root,ori,des,model="unconstrained"):
-    
+def paste_file(root, ori, des, model="unconstrained"):
     # Create subfolders 'mc_1' to 'mc_100' inside the 'calculation' folder
     for i in range(1, 201):
-        mc_subfolder = os.path.join(des, f'mc_{i}')
+        mc_subfolder = os.path.join(des, f"mc_{i}")
         os.makedirs(mc_subfolder, exist_ok=True)
 
     # Copy 'mc_i.csv' files into their respective subfolders
     for i in range(1, 201):
-        mc_csv_file_src = os.path.join(ori, f'mc_{i}.csv')
-        mc_csv_file_dest = os.path.join(des, f'mc_{i}', 'mc_1.csv')
+        mc_csv_file_src = os.path.join(ori, f"mc_{i}.csv")
+        mc_csv_file_dest = os.path.join(des, f"mc_{i}", "mc_1.csv")
         shutil.copy(mc_csv_file_src, mc_csv_file_dest)
 
     # Copy the 'gms' file into each subfolder
     if model == "constrained":
-        target_file="mpc_con.gms"
+        target_file = "mpc_con.gms"
     else:
-        target_file="mpc_uncon.gms"
-        
+        target_file = "mpc_uncon.gms"
+
     gms_file = os.path.join(root, target_file)
     for i in range(1, 201):
-        mc_subfolder = os.path.join(des, f'mc_{i}')
+        mc_subfolder = os.path.join(des, f"mc_{i}")
         shutil.copy(gms_file, mc_subfolder)
 
+    gms_file = os.path.join(des, "GammaData.csv")
+    for i in range(1, 201):
+        mc_subfolder = os.path.join(des, f"mc_{i}")
+        shutil.copy(gms_file, mc_subfolder)
 
-    gms_file = os.path.join(des, 'GammaData.csv')
+    gms_file = os.path.join(des, "ThetaData.csv")
     for i in range(1, 201):
-        mc_subfolder = os.path.join(des, f'mc_{i}')
+        mc_subfolder = os.path.join(des, f"mc_{i}")
         shutil.copy(gms_file, mc_subfolder)
-        
-    gms_file = os.path.join(des, 'ThetaData.csv')
+
+    gms_file = os.path.join(des, "X0Data.csv")
     for i in range(1, 201):
-        mc_subfolder = os.path.join(des, f'mc_{i}')
+        mc_subfolder = os.path.join(des, f"mc_{i}")
         shutil.copy(gms_file, mc_subfolder)
-        
-        
-    gms_file = os.path.join(des, 'X0Data.csv')
+
+    gms_file = os.path.join(des, "Z0Data.csv")
     for i in range(1, 201):
-        mc_subfolder = os.path.join(des, f'mc_{i}')
+        mc_subfolder = os.path.join(des, f"mc_{i}")
         shutil.copy(gms_file, mc_subfolder)
-        
-    gms_file = os.path.join(des, 'Z0Data.csv')
+
+    gms_file = os.path.join(des, "ZbarData.csv")
     for i in range(1, 201):
-        mc_subfolder = os.path.join(des, f'mc_{i}')
+        mc_subfolder = os.path.join(des, f"mc_{i}")
         shutil.copy(gms_file, mc_subfolder)
-        
-        
-    gms_file = os.path.join(des, 'ZbarData.csv')
-    for i in range(1, 201):
-        mc_subfolder = os.path.join(des, f'mc_{i}')
-        shutil.copy(gms_file, mc_subfolder)
-    
-    
-    return("movement is done")
+
+    return "movement is done"
