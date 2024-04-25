07d449c4a3096b3fac5c87988845ea84ab14dea6
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-04-25 11:26:29 -0400

    WIP (need to finish!)

--- pysrc/analysis/__init__.py
+++ pysrc/analysis/__init__.py
@@ -56,3 +56,43 @@ def value_decomposition(
         "total_AC": total_AC,
         "total_PV": total_PV,
     }
+
+
+def transfers_decomposition(
+    Z,
+    X,
+    T,
+    pee,
+    pa,
+    b,
+    theta,
+    delta=0.02,
+    kappa=2.094215255,
+):
+    # Compute change in X
+    X_dot = np.diff(X, axis=0)
+
+    NCE_base = [-kappa * np.sum(Z[t + 1]) + X_dot[t] for t in range(30)]
+    total_NCE_base = np.sum(NCE_base) * 100
+
+    NCE = [-kappa * np.sum(Z[t + 1]) + X_dot[t] for t in range(30)]
+    total_NCE = np.sum(NCE) * 100
+
+    NT2 = [
+        -b * (kappa * np.sum(Z[t + 1]) - X_dot[t]) / ((1 + delta) ** t)
+        for t in range(30)
+    ]
+    total_NT2 = np.sum(NT2)
+
+    total_EC = total_NT2 / (total_NCE - total_NCE_base) * 100
+
+    total_NCE = total_NCE.round(2)
+    total_NT2 = total_NT2.round(2)
+    total_EC = total_EC.round(2)
+    return {
+        "pee": pee,
+        "b": b,
+        "total_NCE": total_NCE,
+        "total_NT2": total_NT2,
+        "total_EC": total_EC,
+    }
