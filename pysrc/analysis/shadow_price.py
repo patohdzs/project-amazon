92a07570c1aeef4ae0f903b6b1878546ac2b7d66
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-05-30 12:25:30 -0400

    Refactored 1995 data loading to data service.

--- pysrc/analysis/shadow_price.py
+++ pysrc/analysis/shadow_price.py
@@ -1,71 +1,8 @@
 import numpy as np
-import pandas as pd
-import geopandas as gpd
-from pathlib import Path
+
 from pysrc.optimization import gams, gurobi
-from pysrc.sampling import baseline
-from pysrc.services.file_service import get_path
 from pysrc.sampling import adjusted
-
-def load_site_data_1995(
-    num_sites: int,
-    norm_fac: float = 1e9,
-    data_dir: Path = get_path("data", "hmc"),
-):
-    # Read data file
-    file_path = data_dir / f"hmc_{num_sites}SitesModel.csv"
-    df = pd.read_csv(file_path)
-
-    # Extract information
-    z_1995 = df[f"z_1995_{num_sites}Sites"].to_numpy()
-    z_2008 = df[f"z_2008_{num_sites}Sites"].to_numpy()
-    zbar_1995 = df[f"zbar_1995_{num_sites}Sites"].to_numpy()
-    forest_area_1995 = df[f"forestArea_1995_ha_{num_sites}Sites"].to_numpy()
-
-    # Normalize Z data
-    zbar_1995 /= norm_fac
-    z_1995 /= norm_fac
-    forest_area_1995 /= norm_fac
-
-    # Read municipal level data
-    municipal_theta_df = gpd.read_file(data_dir / "muni_data_theta.geojson")
-    municipal_gamma_df = gpd.read_file(data_dir / "muni_data_gamma.geojson")
-
-    # Read site level data
-    site_theta_2017 = gpd.read_file(data_dir / f"site_{num_sites}_data_theta.geojson")
-    site_gamma_2017 = gpd.read_file(data_dir / f"site_{num_sites}_data_gamma.geojson")
-
-    # Remove geometries
-    site_theta_2017_df = site_theta_2017.iloc[:, :-1]
-    site_gamma_2017_df = site_gamma_2017.iloc[:, :-1]
-    
-    
-    baseline_fit = baseline.sample(
-        model_name="full_model",
-        num_sites=num_sites,
-        iter_sampling=10**4,
-        chains=5,
-        seed=1,
-    )
-        
-    theta = baseline_fit.stan_variable("theta").mean(axis=0)
-    gamma = baseline_fit.stan_variable("gamma").mean(axis=0)
-    
-
-    print(f"Data successfully loaded from {data_dir}")
-    return (
-        zbar_1995,
-        z_1995,
-        forest_area_1995,
-        site_theta_2017_df,
-        site_gamma_2017_df,
-        municipal_theta_df,
-        municipal_gamma_df,
-        z_2008,
-        theta,
-        gamma,
-    )
-
+from pysrc.services.data_service import load_site_data_1995
 
 
 def shadow_price_opt(
@@ -75,13 +12,12 @@ def shadow_price_opt(
     z_2008,
     theta,
     gamma,
-    sitenum = 78,
-    opt = 'gams',
-    timehzn = 200,
+    sitenum=78,
+    opt="gams",
+    timehzn=200,
     pa=41.11,
     pe=7.1,
 ):
-    
     # Computing carbon absorbed in start period
     x0_vals_1995 = gamma * forest_area_1995
 
@@ -106,19 +42,15 @@ def shadow_price_opt(
         pa=pa,
         model="shadow_price",
     )
-    Z=results['Z']
-    z_2008_agg=np.sum(z_2008)/1e9
-    ratio=np.abs((np.sum(Z[13])-z_2008_agg)/z_2008_agg)
-    
+    Z = results["Z"]
+    z_2008_agg = np.sum(z_2008) / 1e9
+    ratio = np.abs((np.sum(Z[13]) - z_2008_agg) / z_2008_agg)
+
     return ratio
 
 
-def shadow_price_cal(sitenum = 78,
-                     pa=41.11,
-                     opt='gams',
-                     model='det',
-                     xi=1):
-    if model=='det':
+def shadow_price_cal(sitenum=78, pa=41.11, opt="gams", model="det", xi=1):
+    if model == "det":
         (
             zbar_1995,
             z_1995,
@@ -129,26 +61,31 @@ def shadow_price_cal(sitenum = 78,
             _,
             z_2008,
             theta,
-            gamma
+            gamma,
         ) = load_site_data_1995(sitenum)
-        
-        
+
         pe_values = np.arange(5, 8, 0.1)
-        
-        results = np.array([shadow_price_opt(    zbar_1995,
-                                            z_1995,
-                                            forest_area_1995,
-                                            z_2008,
-                                            theta,
-                                            gamma,
-                                            sitenum=sitenum,
-                                            opt=opt,
-                                            timehzn=200,
-                                            pa=pa,
-                                            pe=pe,
-                                            ) for pe in pe_values])
-        
-    elif model=='hmc':
+
+        results = np.array(
+            [
+                shadow_price_opt(
+                    zbar_1995,
+                    z_1995,
+                    forest_area_1995,
+                    z_2008,
+                    theta,
+                    gamma,
+                    sitenum=sitenum,
+                    opt=opt,
+                    timehzn=200,
+                    pa=pa,
+                    pe=pe,
+                )
+                for pe in pe_values
+            ]
+        )
+
+    elif model == "hmc":
         (
             zbar_1995,
             z_1995,
@@ -159,60 +96,54 @@ def shadow_price_cal(sitenum = 78,
             _,
             z_2008,
             theta,
-            gamma
-        ) = load_site_data_1995(sitenum)    
-    
-    
-    
+            gamma,
+        ) = load_site_data_1995(sitenum)
+
         pe_values = np.arange(5, 7, 0.1)
         results = []
         for pe in pe_values:
-            
             samples = adjusted.sample(
-            model_name='full_model',
-            xi=xi,
-            pe=pe,
-            pa=pa,
-            weight=0.25,
-            num_sites=sitenum,
-            T=200,
-            optimizer=opt,
-            max_iter=100,
-            final_sample_size=5_000,
-            iter_sampling=1000,
-            iter_warmup=500,
-            show_progress=True,
-            seed=1,
-            inits=0.2,
+                model_name="full_model",
+                xi=xi,
+                pe=pe,
+                pa=pa,
+                weight=0.25,
+                num_sites=sitenum,
+                T=200,
+                optimizer=opt,
+                max_iter=100,
+                final_sample_size=5_000,
+                iter_sampling=1000,
+                iter_warmup=500,
+                show_progress=True,
+                seed=1,
+                inits=0.2,
             )
 
-            theta=np.mean(samples['final_sample'][:,:78],axis=0)
-            gamma=np.mean(samples['final_sample'][:,78:],axis=0)
-            result = shadow_price_opt(    zbar_1995,
-                                                z_1995,
-                                                forest_area_1995,
-                                                z_2008,
-                                                theta,
-                                                gamma,
-                                                sitenum=sitenum,
-                                                opt=opt,
-                                                timehzn=200,
-                                                pa=pa,
-                                                pe=pe,
-                                                ) 
+            theta = np.mean(samples["final_sample"][:, :78], axis=0)
+            gamma = np.mean(samples["final_sample"][:, 78:], axis=0)
+            result = shadow_price_opt(
+                zbar_1995,
+                z_1995,
+                forest_area_1995,
+                z_2008,
+                theta,
+                gamma,
+                sitenum=sitenum,
+                opt=opt,
+                timehzn=200,
+                pa=pa,
+                pe=pe,
+            )
             results.append(result)
-        results=np.array(results)
-
+        results = np.array(results)
 
     min_index = np.argmin(results)
     min_result = results[min_index]
     min_pe = pe_values[min_index]
-    
 
-    return min_result,min_pe
+    return min_result, min_pe
 
-    
 
-    
-min_result,min_pe=shadow_price_cal(sitenum=78)
-print("min_result",min_result,"min_pe",min_pe)
+min_result, min_pe = shadow_price_cal(sitenum=78)
+print("min_result", min_result, "min_pe", min_pe)
