eb45ebc7337935407500a90e31634eff11db0d72
pengyu <pengyu@uchicago.edu>
2024-05-30 12:52:21 -0500

    shadow price table

--- pysrc/analysis/shadow_price.py
+++ pysrc/analysis/shadow_price.py
@@ -80,6 +80,7 @@ def shadow_price_opt(
     timehzn = 200,
     pa=41.11,
     pe=7.1,
+    model = 'det',
 ):
     
     # Computing carbon absorbed in start period
@@ -94,7 +95,11 @@ def shadow_price_opt(
 
     else:
         raise ValueError("Optimizer must be one of ['gurobi', 'gams']")
-
+    
+    
+    if model == "mpc":
+        solve_planner_problem = gams.mpc_shadow_price
+        
     results = solve_planner_problem(
         T=timehzn,
         theta=theta,
@@ -202,7 +207,42 @@ def shadow_price_cal(sitenum = 78,
                                                 ) 
             results.append(result)
         results=np.array(results)
+    
+    if model=='mpc':
+        (
+            zbar_1995,
+            z_1995,
+            forest_area_1995,
+            _,
+            _,
+            _,
+            _,
+            z_2008,
+            theta,
+            gamma
+        ) = load_site_data_1995(sitenum)
+        
+        
+        pe_values = np.arange(6.5, 7.1, 0.05)
+        
+        results = []
+        for pe in pe_values:
+            result = shadow_price_opt(zbar_1995,
+                                    z_1995,
+                                    forest_area_1995,
+                                    z_2008,
+                                    theta,
+                                    gamma,
+                                    sitenum=sitenum,
+                                    opt=opt,
+                                    timehzn=200,
+                                    pa=pa,
+                                    pe=pe,
+                                    model='mpc')
+            results.append(result)
+            print(f"pe: {pe}, result: {result}")
 
+        results = np.array(results)
 
     min_index = np.argmin(results)
     min_result = results[min_index]
@@ -211,8 +251,19 @@ def shadow_price_cal(sitenum = 78,
 
     return min_result,min_pe
 
-    
 
+
+## det 1043 sites
+min_result,det_1043_pe=shadow_price_cal(sitenum=1043,model='det')
+print("min_result",min_result,"min_pe",det_1043_pe)    
+min_result,det_78_pe=shadow_price_cal(sitenum=78,model='det')
+print("min_result",min_result,"min_pe",det_78_pe)    
+
+
+## hmc 78 sites
+min_result,hmc_78_pe=shadow_price_cal(sitenum=78,model='hmc',xi=1)
+print("min_result",min_result,"min_pe",hmc_78_pe)  
     
-min_result,min_pe=shadow_price_cal(sitenum=78)
-print("min_result",min_result,"min_pe",min_pe)
+## mpc 78 sites    
+min_result,mpc_78_pe=shadow_price_cal(sitenum=78,model='mpc')
+print("min_result",min_result,"min_pe",mpc_78_pe)
