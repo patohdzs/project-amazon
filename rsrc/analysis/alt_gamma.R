8f8aa221e5f0024d8e6b9098ea195102f1d9b42c
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-06-21 15:26:45 -0400

    Added old gamma on new gamma reg.

--- rsrc/analysis/alt_gamma.R
+++ rsrc/analysis/alt_gamma.R
@@ -1,3 +1,4 @@
+library(stargazer)
 library(ggplot2)
 library(terra)
 library(sf)
@@ -6,12 +7,9 @@ library(sf)
 ngs_gextent <- rast("data/raw/global_forest_watch/young_forest_sequestration_rate_Griscom_extent.tif")
 ngs <- rast("data/raw/global_forest_watch/sequestration_rate__mean__aboveground__full_extent__Mg_C_ha_yr.tif")
 
-# Load amazon biome grid
-biome_shares <- rast("data/processed/amazon_biome_1043_sites.tif")
-
-grid <- biome_shares %>%
-  as.polygons(dissolve = FALSE) %>%
-  st_as_sf()
+# Load calibration data set
+load("data/calibration/hmc/hmc_1043SitesModel.Rdata")
+grid <- calibration.1043SitesModel
 
 x <- extract(ngs_gextent, grid, fun = mean, na.rm = TRUE)
 y <- extract(ngs, grid, fun = mean, na.rm = TRUE)
@@ -46,3 +44,23 @@ fig_2 <- grid %>%
   )
 
 ggsave(filename = "plots/alt_gamma_fig_2.png", plot = fig_2)
+
+
+fig_3 <- grid %>%
+  ggplot() +
+  geom_sf(aes(fill = gamma_1043Sites), color = "white") +
+  scale_fill_viridis_c(option = "plasma", na.value = "grey") +
+  labs(
+    title = "Carbon Sequestration Rate",
+    fill = "Mg carbon/ha",
+    x = "Longitude",
+    y = "Latitude"
+  )
+
+ggsave(filename = "plots/gamma.png", plot = fig_3)
+
+# Compare old and new gammas
+model_1 <- lm(gamma_1043Sites ~ new_gamma, data = grid)
+model_2 <- lm(scale(gamma_1043Sites) ~ scale(new_gamma), data = grid)
+stargazer(model_1, model_2, out = "plots/table_1.tex")
+stargazer(model_1, model_2, out = "plots/table_1.txt")
