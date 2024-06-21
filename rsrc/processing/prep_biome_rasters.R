c0c71e4af507eab5bf2f40976ab4e9f9d9fd863e
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-06-21 11:56:59 -0400

    Changes to biome and land use raster prep scripts.

--- rsrc/processing/prep_biome_rasters.R
+++ rsrc/processing/prep_biome_rasters.R
@@ -20,51 +20,51 @@ conflicts_prefer(terra::extract())
 tic(msg = "prep_biome_grid.R script", log = TRUE)
 
 # Load MapBiomas land use data (30m minicells)
-raster_biome <- rast("data/raw/mapbiomas/land_use_cover/COLECAO_5_DOWNLOADS_COLECOES_ANUAL_AMAZONIA_AMAZONIA-2017.tif")
+biome_raster <- rast("data/raw/mapbiomas/land_use_cover/COLECAO_5_DOWNLOADS_COLECOES_ANUAL_AMAZONIA_AMAZONIA-2017.tif")
 
 # Load amazon biome data
 load("data/clean/amazon_biome.Rdata")
 
 # Change biome crs to match raster
-amazon_biome <- st_transform(amazon_biome, crs(raster_biome))
+amazon_biome <- st_transform(amazon_biome, crs(biome_raster))
 
 # Rasterize amazon biome into 30m raster resolution to minimize area distortion
-raster_biome <- rasterize(vect(amazon_biome), raster_biome, field = 1)
+biome_raster <- rasterize(vect(amazon_biome), biome_raster, field = 1)
 
 # Clean environment
 rm(amazon_biome)
 
 # Aggregate raster calculating the share of minicells that are in the biome
 # 2250^2 is the total number of minicells
-raster_biome <- aggregate(
-  raster_biome,
+biome_raster <- aggregate(
+  biome_raster,
   fact = 2250,
   fun = sum,
   na.rm = TRUE
 ) / (2250^2)
 
 # Add name
-names(raster_biome) <- "share_amazon_biome"
+names(biome_raster) <- "share_amazon_biome"
 
-# Save raster
-out_file <- "data/processed/amazon_biome_1043_sites.tif"
-writeRaster(raster_biome, out_file, overwrite = TRUE)
+# Save biome shares raster
+out_file <- "data/processed/amazon_biome_shares_1043_sites.tif"
+writeRaster(biome_raster, out_file, overwrite = TRUE)
 
 # Convert cell size to hectares
-raster_biome_ha <- terra::cellSize(raster_biome, unit = "ha")
+biome_raster_ha <- terra::cellSize(biome_raster, unit = "ha")
 
 # Clean environment
-rm(raster_biome)
+rm(biome_raster)
 
 # Add layer name
-names(raster_biome_ha) <- "pixel_area_ha"
+names(biome_raster_ha) <- "pixel_area_ha"
 
-# Save raster
+# Save areas raster
 out_file <- "data/processed/amazon_biome_areas_1043_sites.tif"
-terra::writeRaster(raster_biome_ha, , overwrite = TRUE)
+terra::writeRaster(biome_raster_ha, , overwrite = TRUE)
 
 # Clean environment
-rm(raster_biome_ha)
+rm(biome_raster_ha)
 
 # End timer
 toc(log = TRUE)
