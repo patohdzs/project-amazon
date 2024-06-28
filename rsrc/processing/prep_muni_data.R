a8d3aa6a1ecb7962b7c4a3578364516408da82f4
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-06-28 15:48:01 -0400

    Removed code related to 2006 cattle slaughter.

--- rsrc/processing/prep_muni_data.R
+++ rsrc/processing/prep_muni_data.R
@@ -29,23 +29,14 @@ load("data/processed/muni_biomass_2017.Rdata")
 # Load spatial municipality-level sample
 load("data/processed/spatial_muni_sample.Rdata")
 
-# 2006 AG CENSUS CATTLE FOR SLAUGHTER
-load("data/clean/cattle_slaughter_2006.Rdata")
-
-# 2017 AG CENSUS CATTLE SOLD
+# Load 2017 cattle output data
 load("data/clean/cattle_sold_2017.Rdata")
 
-# 2006 AG CENSUS AGRICULTURAL USE AREA
-load("data/clean/use_area_2006.Rdata")
-
-# 2017 AG CENSUS AGRICULTURAL USE AREA
+# Load 2017 agricultural use area data
 load("data/clean/use_area_2017.Rdata")
 
 # LAND COVER AND USE (MAPBIOMAS - MUNI LEVEL)
 load("data/clean/land_use_cover_muni.Rdata")
-
-# DEFLATOR (IPA-EP-DI)
-load("data/clean/deflator.Rdata")
 
 # HISTORICAL TEMPERATURE
 raster_temp <- rast("data/clean/temperature.tif")
@@ -59,40 +50,8 @@ muni_biomass_2017 <- muni_biomass_2017 %>%
   summarise(agb_2017 = mean(agb_2017, na.rm = TRUE)) %>%
   st_set_geometry(NULL)
 
-# AGGREGATE DEFLATOR BY YEAR
-deflator <-
-  deflator %>%
-  mutate(year = year(date)) %>%
-  group_by(year) %>%
-  summarise(deflator_ipa = mean(deflator_ipa)) %>%
-  ungroup() %>%
-  filter(year >= 2000)
-
-# CHANGE DEFLATOR BASE TO 2017
-aux_deflator_2017 <- deflator[deflator$year == 2017, ]$deflator_ipa
-
-deflator <-
-  deflator %>%
-  mutate(deflator_ipa = deflator_ipa / aux_deflator_2017)
-
-rm(aux_deflator_2017)
-
 # Commercial exchange rate - selling - average - annual - 2017 - ipeadata
 brl_to_usd <- 3.192
-
-# Deflate cattle slaughter value and
-# change from thousand-BRL to USD
-aux_deflator_2006 <- deflator[deflator$year == 2006, ]$deflator_ipa
-
-cattle_slaughter_2006 <-
-  cattle_slaughter_2006 %>%
-  mutate(slaughter_value_2006 = cattleSlaughter2006_value / aux_deflator_2006) %>%
-  mutate(slaughter_value_2006 = 1000 * slaughter_value_2006 / brl_to_usd) %>%
-  rename(slaughter_head_2006 = cattleSlaughter2006_head) %>%
-  select(muni_code, slaughter_value_2006, slaughter_head_2006)
-
-# Remove deflator
-rm(deflator)
 
 # CONVERT CATTLE SOLD VALUE TO USD
 # change from thousand BRL to BRL to USD
@@ -112,16 +71,6 @@ use_area_2017 <-
     pasture_area_2017,
     crop_area_2017
   )
-
-use_area_2006 <-
-  use_area_2006 %>%
-  select(
-    muni_code,
-    agUse_area_2006,
-    pasture_area_2006,
-    crop_area_2006
-  )
-
 
 # Match spatial sample crs with raster
 spatial_muni_sample <-
@@ -169,8 +118,6 @@ muni_data <-
   spatial_muni_sample %>%
   left_join(cattle_sold_2017, by = c("muni_code")) %>%
   left_join(use_area_2017, by = c("muni_code")) %>%
-  left_join(cattle_slaughter_2006, by = c("muni_code")) %>%
-  left_join(use_area_2006, by = c("muni_code")) %>%
   left_join(muni_biomass_2017, by = c("muni_code"))
 
 # Remove municipalities outside Amazon biome
@@ -180,22 +127,16 @@ muni_data <- muni_data %>% filter(!is.na(biomeAmazon_share))
 # Create agricultural census variables
 # NOTE: average cattle weights 225 kg
 # NOTE 2: USD/@ (average cattle weight 15@)
-muni_data <-
-  muni_data %>%
+muni_data <- muni_data %>%
   mutate(
     slaughter_value_per_ha_2017 = if_else(pasture_area_2017 == 0 & !is.na(slaughter_value_2017), 0, slaughter_value_2017 / pasture_area_2017),
-    slaughter_value_per_ha_2006 = if_else(pasture_area_2006 == 0 & !is.na(slaughter_value_2006), 0, slaughter_value_2006 / pasture_area_2006),
     carcass_weight_per_ha_2017 = if_else(pasture_area_2017 == 0, as.numeric(NA), 225 * slaughter_head_2017 / pasture_area_2017),
-    carcass_weight_per_ha_2006 = if_else(pasture_area_2006 == 0, as.numeric(NA), 225 * slaughter_head_2006 / pasture_area_2006),
     farm_gate_price_2017 = if_else(slaughter_head_2017 == 0, as.numeric(NA), slaughter_value_2017 / (slaughter_head_2017 * 15)),
-    farm_gate_price_2006 = if_else(slaughter_head_2006 == 0, as.numeric(NA), slaughter_value_2006 / (slaughter_head_2006 * 15))
   )
 
 # Clear environment
-rm(use_area_2006)
 rm(use_area_2017)
 rm(cattle_sold_2017)
-rm(cattle_slaughter_2006)
 rm(spatial_muni_sample)
 
 # Set labels
@@ -207,10 +148,6 @@ set_label(muni_data$agUse_area_2017) <- "agricultural use area (ha, 2017 Ag Cens
 set_label(muni_data$carcass_weight_per_ha_2017) <- "total carcass weigth of cattle sold for slaughter per pasture area (kg/ha, 2017 Ag Census)"
 set_label(muni_data$slaughter_value_per_ha_2017) <- "value of cattle sold for slaughter per pasture area (2017 constantUSD/ha, 2017 Ag Census)"
 set_label(muni_data$farm_gate_price_2017) <- "farm gate price cattle sold for slaughter (2017 constant USD/@, 2017 Ag Census)"
-set_label(muni_data$agUse_area_2006) <- "agricultural use area (ha)"
-set_label(muni_data$carcass_weight_per_ha_2006) <- "total carcass weigth of cattle sold for slaughter per pasture area (kg/ha, 2017 Ag Census)"
-set_label(muni_data$slaughter_value_per_ha_2006) <- "value of cattle sold for slaughter per pasture area (2017 constantUSD/ha, 2017 Ag Census)"
-set_label(muni_data$farm_gate_price_2006) <- "farm gate price cattle sold for slaughter (2017 constant USD/@, 2017 Ag Census)"
 
 
 # Interpolate missing values of farm gate price (alternative approach)
