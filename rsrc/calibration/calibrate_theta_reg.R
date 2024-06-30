853c635e9cd6efffdf358e71bba0f40a0d906f3e
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-06-30 19:19:03 -0400

    Cleaned up scripts for theta and gamma regs.

--- rsrc/calibration/calibrate_theta_reg.R
+++ rsrc/calibration/calibrate_theta_reg.R
@@ -1,44 +1,36 @@
+library(sf)
+library(tictoc)
+library(readxl)
+library(tidyverse)
 
-tictoc::tic(msg = "theta.R script", log = TRUE)
+tictoc::tic(msg = "calibrate_theta_reg.R script", log = TRUE)
 
-# DATA INPUT
-# load variables at the muni level to calibrate theta
-load("data/prepData/muniTheta_prepData.Rdata")
+# Load variables at the muni level to calibrate theta
+load("data/processed/muni_data.Rdata")
 
-# load cattle price series
-load("data/prepData/seriesPriceCattle_prepData.Rdata")
-
-# load distance data
+# Load distance data
 distance_data <-
   read_excel("data/raw/ipea/distance_to_capital/ipeadata[21-08-2023-01-28].xls") %>%
   mutate(muni_code = as.numeric(muni_code))
 
-
-# DATA MANIPULATION
-
 # Drop outliers and separate geometry from df
-muni_theta_prep_data <-
-  as.data.frame(muniTheta_prepData)[-c(142, 106, 112), ]
+geo <- st_geometry(muni_data)[-c(142, 106, 112)]
+muni_data <- as.data.frame(muni_data)[-c(142, 106, 112), ]
 
-geo <- st_geometry(muniTheta_prepData)[-c(142, 106, 112)]
-
-# Merge with distance data and farm price data
-muni_theta_prep_data <- muni_theta_prep_data %>%
+# Merge with distance data
+muni_data <- muni_data %>%
   left_join(distance_data, by = "muni_code") %>%
   st_sf(geometry = geo) %>%
-  mutate(
-    cattle_price_2017 = cattleSlaughter_farmGatePrice_2017
-  ) %>%
   dplyr::filter(!is.na(distance))
 
 # Add vector of ones, add polynomial terms, and scale
-df <- muni_theta_prep_data %>%
+df <- muni_data %>%
   mutate(
     X1 = 1,
     historical_temp_sq = historical_temp^2,
     lat_sq = lat^2,
-    log_cattleSlaughter_valuePerHa_2017 = log(cattleSlaughter_valuePerHa_2017),
-    log_cattle_price = log(cattle_price_2017),
+    log_slaughter_value_per_ha = log(slaughter_value_per_ha_2017),
+    log_farm_gate_price = log(farm_gate_price_2017),
     weights = pasture_area_2017
   ) %>%
   mutate(across(
@@ -48,7 +40,7 @@ df <- muni_theta_prep_data %>%
       historical_temp_sq,
       lat,
       lat_sq,
-      log_cattle_price,
+      log_farm_gate_price,
       distance
     ),
     scale
@@ -60,37 +52,37 @@ df <- muni_theta_prep_data %>%
     historical_temp_sq,
     lat,
     lat_sq,
-    log_cattle_price,
+    log_farm_gate_price,
     distance,
-    log_cattleSlaughter_valuePerHa_2017,
+    log_slaughter_value_per_ha,
     weights
   )
 
 # Output municipality-level regression data
 st_write(df,
-  "data/calibration/hmc/muni_data_theta.geojson",
+  "data/calibration/hmc/theta_reg_muni_data.geojson",
   driver = "GeoJSON",
   delete_dsn = TRUE
 )
 
 # Output site-level regression data
-for (n in list(10, 24, 40, 78,1043)) {
+for (n in list(78, 1043)) {
   id_df <- st_read(sprintf("data/calibration/hmc/id_%d.geojson", n))
 
   # Project data to site level
-  site_theta2017 <- df %>%
+  site_level_df <- df %>%
     st_intersection(id_df)
 
   # Set area units to hectares
-  site_theta2017$muni_site_area <-
-    st_area(site_theta2017) %>%
+  site_level_df$muni_site_area <-
+    st_area(site_level_df) %>%
     units::set_units(ha) %>%
     unclass()
 
   # Write to file
   st_write(
-    site_theta2017,
-    sprintf("data/calibration/hmc/site_%d_data_theta.geojson", n),
+    site_level_df,
+    sprintf("data/calibration/hmc/theta_reg_%d_sites_data.geojson", n),
     driver = "GeoJSON",
     delete_dsn = TRUE
   )
