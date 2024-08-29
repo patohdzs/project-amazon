f3da86eadd8b04e6a5f7ce666de40e4b05e344e8
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-08-29 15:07:39 -0400

    Update 78 site calibration to add pasture quality.

--- rsrc/calibration/calibrate_78_sites_model.R
+++ rsrc/calibration/calibrate_78_sites_model.R
@@ -38,7 +38,7 @@ load("data/processed/cattle_price_index.Rdata")
 rsts <- rast(
   list.files(
     "data/processed/",
-    pattern = "amazon_",
+    pattern = ".tif",
     full.names = TRUE
   )
 )
@@ -58,6 +58,15 @@ rsts$other_area_1995_ha <-
 
 rsts$forest_area_2017_ha <-
   rsts$share_forest_2017 * rsts$pixel_area_ha
+
+rsts$area_pasture_quality_1_2017 <-
+  rsts$share_pasture_quality_1_2017 * rsts$pixel_area_ha
+
+rsts$area_pasture_quality_2_2017 <-
+  rsts$share_pasture_quality_2_2017 * rsts$pixel_area_ha
+
+rsts$area_pasture_quality_3_2017 <-
+  rsts$share_pasture_quality_3_2017 * rsts$pixel_area_ha
 
 rsts$agricultural_use_area_2017_ha <-
   rsts$share_agricultural_use_2017 * rsts$pixel_area_ha
@@ -94,7 +103,7 @@ calib_df <- calib_df %>%
 # Add id variable
 calib_df$id <- seq_len(nrow(calib_df))
 
-# Transform share variables into area (ha)
+# Change names to z and add zbar
 calib_df <- calib_df %>%
   rename(site_area_ha = pixel_area_ha) %>%
   mutate(
@@ -104,8 +113,19 @@ calib_df <- calib_df %>%
     zbar_1995 = forest_area_1995_ha + z_1995,
     zbar_2008 = forest_area_2008_ha + z_2008,
     zbar_2017 = forest_area_2017_ha + z_2017,
+  )
+
+# Compute bad pasture quality shares
+calib_df <- calib_df %>%
+  mutate(
+    area_pasture_2017 = area_pasture_quality_1_2017 +
+      area_pasture_quality_2_2017 +
+      area_pasture_quality_3_2017
   ) %>%
-  select(id, contains("area"), contains("z"))
+  mutate(
+    share_low_pq = area_pasture_quality_1_2017 / area_pasture_2017,
+  ) %>%
+  select(id, contains("area"), contains("z"), contains("pq"))
 
 # Convert biomass to CO2 equivalent
 muni_data <- muni_data %>%
