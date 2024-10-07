8f4f47ae67360b3169a06b0f3fcb7def5620731c
pengyu <pengyu@uchicago.edu>
2024-10-06 22:34:46 -0500

    update value_decomposition

--- pysrc/analysis/__init__.py
+++ pysrc/analysis/__init__.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def value_decomposition(
     Z,
     X,
@@ -11,12 +10,10 @@ def value_decomposition(
     pa,
     b,
     theta,
-    low_pq,
     delta=0.02,
     kappa=2.094215255,
-    zeta_1=1.66e-4 * 1e9,
-    zeta_2=0.1473979 * 1e9,
-    zeta_3=0.00010203 * 1e9,
+    zeta_u=1.66e-4 * 1e9,
+    zeta_v=1.00e-4 * 1e9,
 ):
     # Compute change in X
     X_dot = np.diff(X, axis=0)
@@ -41,12 +38,7 @@ def value_decomposition(
 
     # Compute adjustment costs
     results_AC = [
-        (
-            (zeta_1 / 2) * (U[t].sum() ** 2)
-            + (zeta_2 / 2) * ((low_pq * V[t]).sum() ** 2)
-            + (zeta_3 / 2) * ((1 - low_pq * V[t]).sum() ** 2)
-        )
-        / ((1 + delta) ** t)
+        ((zeta_u / 2) * (np.sum(U[t])) ** 2 + (zeta_v / 2) * (np.sum(V[t])) ** 2 )/ ((1 + delta) ** t)
         for t in range(T)
     ]
     total_AC = np.sum(results_AC)
