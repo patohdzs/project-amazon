f50e59b7f7313fd99ae4f7a94e384e1219b5ad45
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2024-10-07 17:03:41 -0400

    Added function to save results + fixed filepaths.

--- pysrc/services/get_opt.py
+++ pysrc/services/get_opt.py
@@ -1,11 +1,10 @@
-import os
 import pickle
-import shutil
-import numpy as np
-import pandas as pd
+from pathlib import Path
 
-from pysrc.optimization import solve_planner_problem
-from pysrc.services.data_service import load_site_data
+import numpy as np
+
+from pysrc.optimization import PlannerSolution, solve_planner_problem
+from pysrc.services.data_service import load_productivity_params, load_site_data
 from pysrc.services.file_service import get_path
 
 
@@ -24,30 +23,17 @@ def get_optimization(
     ) = load_site_data(num_sites)
 
     if model == "det":
-        # Set initial theta & gamma using baseline mean
-        # baseline_fit = baseline.sample(
-        #     num_sites=num_sites, iter_sampling=10**4, chains=5, seed=1
-        # )
-        theta_vals = (
-            pd.read_csv(
-                get_path("data", "calibration", "hmc") / f"theta_fit_{num_sites}.csv"
-            )
-            .to_numpy()[:,]
-            .flatten()
-        )
-        gamma_vals = (
-            pd.read_csv(
-                get_path("data", "calibration", "hmc") / f"gamma_fit_{num_sites}.csv"
-            )
-            .to_numpy()[:,]
-            .flatten()
-        )
+        # Load baseline productivity params
+        (theta_vals, gamma_vals) = load_productivity_params(num_sites)
+
+        # Compute initial carbon stock
         x0_vals = gamma_vals * forest_area_2017
 
+        # Solve for all transfer levels
         b = [0, 10, 15, 20, 25]
         pe_values = [pee + bi for bi in b]
         for pe in pe_values:
-            results=solve_planner_problem(
+            results = solve_planner_problem(
                 time_horizon=200,
                 theta=theta_vals,
                 gamma=gamma_vals,
@@ -59,45 +45,44 @@ def get_optimization(
             )
 
             print("Results for pe = ", pe)
-
-            output_folder = os.path.join(
-                str(get_path("output")),
-                "optimization",
-                model,
-                solver,
-                f"{num_sites}sites",
-                f"pa_{pa}",
-                f"pe_{pe}",
+            output_folder = (
+                get_path("output")
+                / "optimization"
+                / model
+                / solver
+                / f"{num_sites}sites"
+                / f"pa_{pa}"
+                / f"pe_{pe}"
             )
 
-            if not os.path.exists(output_folder):
-                os.makedirs(output_folder)
-                
-            np.savetxt(os.path.join(output_folder, "Z.txt"), results.Z, delimiter=",")
-            np.savetxt(os.path.join(output_folder, "X.txt"), results.X, delimiter=",")
-            np.savetxt(os.path.join(output_folder, "U.txt"), results.U, delimiter=",")
-            np.savetxt(os.path.join(output_folder, "V.txt"), results.V, delimiter=",")
-
+            save_planner_solution(results, output_folder)
 
     if model == "hmc":
-        result_folder = os.path.join(
-            str(get_path("output")),
-            "sampling",
-            solver,
-            f"{num_sites}sites",
-            f"pa_{pa}",
-            f"xi_{xi}",
+        results_dir = (
+            get_path("output")
+            / "sampling"
+            / solver
+            / f"{num_sites}sites"
+            / f"pa_{pa}"
+            / f"xi_{xi}"
         )
+
+        # Solve model for all transfer values
         b = [0, 10, 15, 20, 25]
         pe_values = [pee + bi for bi in b]
         for pe in pe_values:
-            with open(result_folder + f"/pe_{pe}/results.pcl", "rb") as f:
+            # Load ambiguity-adjusted params
+            with open(results_dir / f"/pe_{pe}/results.pcl", "rb") as f:
                 b = pickle.load(f)
+
+            # Take sample
             theta_vals = b["final_sample"][:16000, :78].mean(axis=0)
             gamma_vals = b["final_sample"][:16000, 78:].mean(axis=0)
+
+            # Compute carbon stock
             x0_vals = gamma_vals * forest_area_2017
 
-            results=solve_planner_problem(
+            results = solve_planner_problem(
                 time_horizon=200,
                 theta=theta_vals,
                 gamma=gamma_vals,
@@ -109,21 +94,22 @@ def get_optimization(
             )
             print("Results for pe = ", pe)
 
-            output_folder = os.path.join(
-                str(get_path("output")),
-                "optimization",
-                model,
-                solver,
-                f"{num_sites}sites",
-                f"pa_{pa}",
-                f"pe_{pe}",
+            output_folder = (
+                get_path("output")
+                / "optimization"
+                / model
+                / solver
+                / f"{num_sites}sites"
+                / f"pa_{pa}"
+                / f"pe_{pe}"
             )
 
-            if not os.path.exists(output_folder):
-                os.makedirs(output_folder)
-                
-            np.savetxt(os.path.join(output_folder, "Z.txt"), results.Z, delimiter=",")
-            np.savetxt(os.path.join(output_folder, "X.txt"), results.X, delimiter=",")
-            np.savetxt(os.path.join(output_folder, "U.txt"), results.U, delimiter=",")
-            np.savetxt(os.path.join(output_folder, "V.txt"), results.V, delimiter=",")
+            save_planner_solution(results, output_folder)
     return
+
+
+def save_planner_solution(results: PlannerSolution, output_dir: Path):
+    np.savetxt(output_dir / "Z.txt", results.Z, delimiter=",")
+    np.savetxt(output_dir / "X.txt", results.X, delimiter=",")
+    np.savetxt(output_dir / "U.txt", results.U, delimiter=",")
+    np.savetxt(output_dir / "V.txt", results.V, delimiter=",")
