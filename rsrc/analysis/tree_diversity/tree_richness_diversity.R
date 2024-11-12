eddcdc21a50c6b82776c4ab88c716fd44aaf6eee
ruangkawprc <114883377+ruangkawprc@users.noreply.github.com>
2024-11-11 19:43:26 -0500

    Tree Richness and Diversity

---
+++ rsrc/analysis/tree_diversity/tree_richness_diversity.R
@@ -0,0 +1,24 @@
+
+library(raster)
+library(sp)
+
+# Tree species-richness (species/ha) Plot
+tree_richness <- raster("data/raw/tree_diversity/TreeRichness_ha.asc")
+
+tree_richness[tree_richness == -9999] <- NA
+png("plots/tree_diversity/tree_richness_plot.png", width = 800, height = 600)
+
+plot(tree_richness, col = terrain.colors(100), main = "Tree Species-Richness (species/ha) Amazon Rainforest", axes = TRUE)
+
+dev.off()
+
+# Tree alpha-diversity (Fisher's alpha) Plot
+# S=a*ln(1+n/a) where S is number of taxa, n is number of individuals and a is the Fisher's alpha
+tree_richness <- raster("data/raw/tree_diversity/TreeDiversity.asc")
+
+tree_richness[tree_richness == -9999] <- NA
+png("plots/tree_diversity/tree_diversity_plot.png", width = 800, height = 600)
+
+plot(tree_richness, col = terrain.colors(100), main = "Tree alpha-diversity (Fisher's alpha) in Amazon Rainforest", axes = TRUE)
+
+dev.off()
