f5e114bb62a241da6576687e0320d029d57ed08a
korito1416 <133778874+korito1416@users.noreply.github.com>
2025-01-15 10:52:40 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="project_amazon",
+    version="0.1",
+    packages=find_packages(),  # Automatically discovers all submodules, including pysrc
+    install_requires=[],
+)
